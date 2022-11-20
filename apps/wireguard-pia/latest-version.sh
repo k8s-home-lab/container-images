a31b052195238a8f687a348aef62f8bf1f00ba89
Francesco Astegiano <francesco.astegiano@gmail.com>
2022-11-20 15:19:13 +0100

    [wireguard-pia] update version fetcher

--- apps/wireguard-pia/latest-version.sh
+++ apps/wireguard-pia/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repospia-foss/manual-connections/tags" | jq --raw-output '.[0].name')
+version=$(curl -sX GET "https://api.github.com/repos/pia-foss/manual-connections/tags" | jq --raw-output '.[0].name')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
